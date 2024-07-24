6a7825a7e1bb5ddece392c89df06eb35e1bbbde6
Jarkko Toivonen <jarkko.toivonen@veripalvelu.fi>
2024-07-24 11:24:59 +0300

    Bumped version number

--- src/docker-apps-plumber.R
+++ src/docker-apps-plumber.R
@@ -4,7 +4,7 @@
 # Start in src directory with
 # Rscript docker-server-plumber.R
 
-container_version="0.35"
+container_version="0.36"
 cat(container_version, file = "../output/version.txt")
 zip_file <- sprintf("results-%s.zip", container_version)
 
